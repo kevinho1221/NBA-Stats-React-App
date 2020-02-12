90c36a15b6493b648ef961d2919f0debb9ced481
Kevin Ho <kevinho122195@gmail.com>
2020-02-12 17:25:00 -0500

    In the process of making the suggestions not show inactive players. Fixed an issue where more than one instance of a players name would be displayed and when the suggestion would not display if only one player object was searched. Fixed an issue where the app woudl break if the spelling of a player name was wrong

--- src/Search/searchbar.js
+++ src/Search/searchbar.js
@@ -75,10 +75,33 @@ class searchbar extends React.Component {
     }
   };*/
 
-  checkActive = (num, totalcount, origtotalcount) => {
+  checkActive = (
+    num,
+    totalcount,
+    origtotalcount,
+    numOfSuggestions,
+    checkFirst
+  ) => {
+    //check to see if there is only one search result, if there is then set the
+    //suggestions to show only 1 item
+    if (checkFirst == true) {
+      if (totalcount == 1) {
+        return {
+          num: num,
+          count: totalcount,
+          name: this.state.suggestions.data[num].last_name,
+          suggestions: 1
+        };
+      }
+    }
     //checking to see if totalcount is 0 because it means there are no valid suggestions left
     if (totalcount == 0 || num >= origtotalcount - 1) {
-      return { num: 0, count: 0, name: "None" };
+      return {
+        num: num,
+        count: totalcount,
+        name: this.state.suggestions.data[num].last_name,
+        suggestions: numOfSuggestions
+      };
     }
     var i = num + 1;
     var height = this.state.suggestions.data[i].height_feet;
@@ -103,7 +126,8 @@ class searchbar extends React.Component {
     return {
       num: i,
       count: totalcount,
-      name: this.state.suggestions.data[i].last_name
+      name: this.state.suggestions.data[i].last_name,
+      suggestions: numOfSuggestions + 1
     };
   };
 
@@ -113,70 +137,102 @@ class searchbar extends React.Component {
     var start = 0;
     var totalcount = data.meta.total_count;
     var origtotalcount = data.meta.total_count;
+    var numOfSuggestions = 0;
 
-    const checka = this.checkActive(start, totalcount, origtotalcount);
+    if (totalcount == 0) {
+      return;
+    }
+    const checka = this.checkActive(
+      start,
+      totalcount,
+      origtotalcount,
+      numOfSuggestions,
+      true
+    );
     var a = checka.num;
     totalcount = checka.count;
+    numOfSuggestions = checka.suggestions;
+    console.log(a, totalcount, checka.name, numOfSuggestions);
 
-    console.log(a, totalcount, checka.name);
-
-    const checkb = this.checkActive(a, totalcount, origtotalcount);
+    const checkb = this.checkActive(
+      a,
+      totalcount,
+      origtotalcount,
+      numOfSuggestions
+    );
     var b = checkb.num;
     totalcount = checkb.count;
+    numOfSuggestions = checkb.suggestions;
+    console.log(b, totalcount, checkb.name, numOfSuggestions);
 
-    console.log(b, totalcount, checkb.name);
-
-    const checkc = this.checkActive(b, totalcount, origtotalcount);
+    const checkc = this.checkActive(
+      b,
+      totalcount,
+      origtotalcount,
+      numOfSuggestions
+    );
     var c = checkc.num;
     totalcount = checkc.count;
+    numOfSuggestions = checkc.suggestions;
+    console.log(c, totalcount, checkc.name, numOfSuggestions);
 
-    console.log(c, totalcount, checkc.name);
-
-    const checkd = this.checkActive(c, totalcount, origtotalcount);
+    const checkd = this.checkActive(
+      c,
+      totalcount,
+      origtotalcount,
+      numOfSuggestions
+    );
     var d = checkd.num;
     totalcount = checkd.count;
+    numOfSuggestions = checkd.suggestions;
+    console.log(d, totalcount, checkd.name, numOfSuggestions);
 
-    console.log(d, totalcount, checkd.name);
-
-    const checke = this.checkActive(d, totalcount, origtotalcount);
+    const checke = this.checkActive(
+      d,
+      totalcount,
+      origtotalcount,
+      numOfSuggestions
+    );
     var e = checke.num;
     totalcount = checke.count;
-
-    console.log(e, totalcount, checke.name);
+    numOfSuggestions = checke.suggestions;
+    console.log(e, totalcount, checke.name, numOfSuggestions);
 
     this.setState({ isOpen: true });
-    if (totalcount >= 5) {
+
+    console.log(numOfSuggestions);
+    if (numOfSuggestions >= 5) {
       this.setState({ sugg1: this.state.suggestions.data[a] });
       this.setState({ sugg2: this.state.suggestions.data[b] });
       this.setState({ sugg3: this.state.suggestions.data[c] });
       this.setState({ sugg4: this.state.suggestions.data[d] });
       this.setState({ sugg5: this.state.suggestions.data[e] });
-    } else if (totalcount === 4) {
+    } else if (numOfSuggestions === 4) {
       this.setState({ sugg1: this.state.suggestions.data[a] });
       this.setState({ sugg2: this.state.suggestions.data[b] });
       this.setState({ sugg3: this.state.suggestions.data[c] });
       this.setState({ sugg4: this.state.suggestions.data[d] });
       this.setState({ sugg5: [] });
-    } else if (totalcount === 3) {
+    } else if (numOfSuggestions === 3) {
       this.setState({ sugg1: this.state.suggestions.data[a] });
       this.setState({ sugg2: this.state.suggestions.data[b] });
       this.setState({ sugg3: this.state.suggestions.data[c] });
       this.setState({ sugg4: [] });
       this.setState({ sugg5: [] });
-    } else if (totalcount === 2) {
+    } else if (numOfSuggestions === 2) {
       this.setState({ sugg1: this.state.suggestions.data[a] });
       this.setState({ sugg2: this.state.suggestions.data[b] });
       this.setState({ sugg3: [] });
       this.setState({ sugg4: [] });
       this.setState({ sugg5: [] });
-    } else if (totalcount === 1) {
+    } else if (numOfSuggestions === 1) {
       this.setState({ sugg1: this.state.suggestions.data[a] });
       this.setState({ sugg2: [] });
       this.setState({ sugg3: [] });
       this.setState({ sugg4: [] });
       this.setState({ sugg5: [] });
     } else {
-      this.setState({ sugg1: this.state.suggestions.data[a] });
+      this.setState({ sugg1: [] });
       this.setState({ sugg2: [] });
       this.setState({ sugg3: [] });
       this.setState({ sugg4: [] });

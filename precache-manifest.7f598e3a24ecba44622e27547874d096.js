bc8cec42f436c5a8c34b2be8ad54b8403badeb06
Kevin Ho <kevinho122195@gmail.com>
2020-02-20 15:57:24 -0500

    Updates

---
+++ precache-manifest.7f598e3a24ecba44622e27547874d096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e52560ebb618555d00748e1b7273eb",
+    "url": "/NBA-Stats-React-App/index.html"
+  },
+  {
+    "revision": "36632d94f5e2dfc6188e",
+    "url": "/NBA-Stats-React-App/static/css/main.7f7850fd.chunk.css"
+  },
+  {
+    "revision": "39cbe8379232c9c6c5aa",
+    "url": "/NBA-Stats-React-App/static/js/2.1e9ac43d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/NBA-Stats-React-App/static/js/2.1e9ac43d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "36632d94f5e2dfc6188e",
+    "url": "/NBA-Stats-React-App/static/js/main.06f7a72d.chunk.js"
+  },
+  {
+    "revision": "d624fbd1465ff7d36e4b",
+    "url": "/NBA-Stats-React-App/static/js/runtime-main.a4e499ac.js"
+  }
+]);
